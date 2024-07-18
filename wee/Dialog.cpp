608b1a52030f724889dc1e2b72ed1a490dca0ad5
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-07-18 14:40:30 -0300

    Isolating Dialog system modal methods.

--- wee/Dialog.cpp
+++ wee/Dialog.cpp
@@ -7,6 +7,106 @@
 #include "Com.h"
 using namespace lib;
 
+static std::vector<COMDLG_FILTERSPEC> _makeFilters(
+	std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts)
+{
+	std::vector<COMDLG_FILTERSPEC> rawFilters; // {L"Word Document (*.doc)", L"*.doc"}
+	rawFilters.reserve(namesExts.size());
+	for (const auto& nameExt : namesExts)
+		rawFilters.emplace_back(nameExt.first.data(), nameExt.second.data());
+	return rawFilters;
+}
+
+static std::wstring _shellItemPath(const ComPtr<IShellItem>& shi)
+{
+	LPWSTR ptrPath = nullptr;
+	if (HRESULT hr = shi->GetDisplayName(SIGDN_FILESYSPATH, &ptrPath); FAILED(hr)) [[unlikely]] {
+		throw std::system_error(hr, std::system_category(), "IShellItem::GetDisplayName failed");
+	}
+	std::wstring strPath{(ptrPath == nullptr ? L"" : ptrPath)};
+	CoTaskMemFree(ptrPath);
+	return strPath;
+}
+
+std::optional<std::vector<std::wstring>> Dialog::SysDlgs::openFiles(
+	std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const
+{
+	std::vector<COMDLG_FILTERSPEC> filters = _makeFilters(namesExts);
+
+	ComPtr<IFileOpenDialog> fsd{CLSID_FileOpenDialog};
+	fsd->SetOptions(FOS_FORCEFILESYSTEM | FOS_FILEMUSTEXIST | FOS_ALLOWMULTISELECT);
+	fsd->SetFileTypes(static_cast<int>(filters.size()), filters.data());
+	fsd->SetFileTypeIndex(1);
+	if (HRESULT hr = fsd->Show(_pDlg->hWnd()); hr == S_OK) {
+		ComPtr<IShellItemArray> shiArr;
+		fsd->GetResults(shiArr.pptr());
+
+		DWORD count = 0;
+		shiArr->GetCount(&count);
+
+		std::vector<std::wstring> strPaths;
+		strPaths.reserve(count);
+		for (size_t i = 0; i < count; ++i) {
+			ComPtr<IShellItem> shi;
+			shiArr->GetItemAt(static_cast<DWORD>(i), shi.pptr());
+			strPaths.emplace_back(_shellItemPath(shi));
+		}
+
+		std::sort(strPaths.begin(), strPaths.end(), [](const auto& a, const auto& b) -> bool {
+			return lstrcmpiW(a.c_str(), b.c_str()) < 1;
+		});
+		return strPaths;
+	} else if (hr == HRESULT_FROM_WIN32(ERROR_CANCELLED)) {
+		return std::nullopt;
+	} else [[unlikely]] {
+		throw std::system_error(hr, std::system_category(), "IModalWindow::Show failed");
+	}
+}
+
+std::optional<std::wstring> Dialog::SysDlgs::_openSave(bool isOpen, bool isFolder,
+	std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const
+{
+	std::vector<COMDLG_FILTERSPEC> filters = _makeFilters(namesExts);
+
+	ComPtr<IFileDialog> fd{(isOpen ? CLSID_FileOpenDialog : CLSID_FileSaveDialog)};
+	fd->SetOptions(FOS_FORCEFILESYSTEM | FOS_FILEMUSTEXIST | (isFolder ? FOS_PICKFOLDERS : 0));
+	if (!isFolder) {
+		fd->SetFileTypes(static_cast<int>(filters.size()), filters.data());
+		fd->SetFileTypeIndex(1);
+	}
+	if (HRESULT hr = fd->Show(_pDlg->hWnd()); hr == S_OK) {
+		ComPtr<IShellItem> shi;
+		fd->GetResult(shi.pptr());
+		return _shellItemPath(shi);
+	} else if (hr == HRESULT_FROM_WIN32(ERROR_CANCELLED)) {
+		return std::nullopt;
+	} else [[unlikely]] {
+		throw std::system_error(hr, std::system_category(), "IModalWindow::Show failed");
+	}
+}
+
+int Dialog::SysDlgs::msgBox(std::wstring_view title, std::wstring_view mainInstruction,
+	std::wstring_view body, int tdcbfButtons, LPWSTR tdIcon) const
+{
+	TASKDIALOGCONFIG tdc = {
+		.cbSize = sizeof(TASKDIALOGCONFIG),
+		.hwndParent = _pDlg->hWnd(),
+		.dwFlags = TDF_ALLOW_DIALOG_CANCELLATION | TDF_POSITION_RELATIVE_TO_WINDOW,
+		.dwCommonButtons = tdcbfButtons,
+		.pszWindowTitle = title.data(),
+		.pszMainIcon = tdIcon,
+		.pszMainInstruction = mainInstruction.empty() ? nullptr : mainInstruction.data(),
+		.pszContent = body.data(),
+	};
+
+	int pnButton = 0;
+	if (HRESULT hr = TaskDialogIndirect(&tdc, &pnButton, nullptr, nullptr); FAILED(hr)) [[unlikely]] {
+		throw std::system_error(hr, std::system_category(), "TaskDialogIndirect failed");
+	}
+	return pnButton;
+}
+
+
 INT_PTR CALLBACK Dialog::_DlgProc(HWND hDlg, UINT uMsg, WPARAM wp, LPARAM lp)
 {
 	Dialog *pSelf = nullptr;
@@ -55,103 +155,4 @@ void Dialog::enable(std::initializer_list<WORD> ctrlIds, bool doEnable) const
 {
 	for (auto&& ctrlId : ctrlIds)
 		EnableWindow(GetDlgItem(hWnd(), ctrlId), doEnable);
-}
-
-static std::vector<COMDLG_FILTERSPEC> _makeFilters(
-	std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts)
-{
-	std::vector<COMDLG_FILTERSPEC> rawFilters; // {L"Word Document (*.doc)", L"*.doc"}
-	rawFilters.reserve(namesExts.size());
-	for (const auto& nameExt : namesExts)
-		rawFilters.emplace_back(nameExt.first.data(), nameExt.second.data());
-	return rawFilters;
-}
-
-static std::wstring _shellItemPath(const ComPtr<IShellItem>& shi)
-{
-	LPWSTR ptrPath = nullptr;
-	if (HRESULT hr = shi->GetDisplayName(SIGDN_FILESYSPATH, &ptrPath); FAILED(hr)) [[unlikely]] {
-		throw std::system_error(hr, std::system_category(), "IShellItem::GetDisplayName failed");
-	}
-	std::wstring strPath{(ptrPath == nullptr ? L"" : ptrPath)};
-	CoTaskMemFree(ptrPath);
-	return strPath;
-}
-
-std::optional<std::vector<std::wstring>> Dialog::showOpenFiles(
-	std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const
-{
-	std::vector<COMDLG_FILTERSPEC> filters = _makeFilters(namesExts);
-
-	ComPtr<IFileOpenDialog> fsd{CLSID_FileOpenDialog};
-	fsd->SetOptions(FOS_FORCEFILESYSTEM | FOS_FILEMUSTEXIST | FOS_ALLOWMULTISELECT);
-	fsd->SetFileTypes(static_cast<int>(filters.size()), filters.data());
-	fsd->SetFileTypeIndex(1);
-	if (HRESULT hr = fsd->Show(hWnd()); hr == S_OK) {
-		ComPtr<IShellItemArray> shiArr;
-		fsd->GetResults(shiArr.pptr());
-
-		DWORD count = 0;
-		shiArr->GetCount(&count);
-
-		std::vector<std::wstring> strPaths;
-		strPaths.reserve(count);
-		for (size_t i = 0; i < count; ++i) {
-			ComPtr<IShellItem> shi;
-			shiArr->GetItemAt(static_cast<DWORD>(i), shi.pptr());
-			strPaths.emplace_back(_shellItemPath(shi));
-		}
-
-		std::sort(strPaths.begin(), strPaths.end(), [](const auto& a, const auto& b) -> bool {
-			return lstrcmpiW(a.c_str(), b.c_str()) < 1;
-		});
-		return strPaths;
-	} else if (hr == HRESULT_FROM_WIN32(ERROR_CANCELLED)) {
-		return std::nullopt;
-	} else [[unlikely]] {
-		throw std::system_error(hr, std::system_category(), "IModalWindow::Show failed");
-	}
-}
-
-std::optional<std::wstring> Dialog::_showOpenSave(bool isOpen, bool isFolder,
-	std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const
-{
-	std::vector<COMDLG_FILTERSPEC> filters = _makeFilters(namesExts);
-
-	ComPtr<IFileDialog> fd{(isOpen ? CLSID_FileOpenDialog : CLSID_FileSaveDialog)};
-	fd->SetOptions(FOS_FORCEFILESYSTEM | FOS_FILEMUSTEXIST | (isFolder ? FOS_PICKFOLDERS : 0));
-	if (!isFolder) {
-		fd->SetFileTypes(static_cast<int>(filters.size()), filters.data());
-		fd->SetFileTypeIndex(1);
-	}
-	if (HRESULT hr = fd->Show(hWnd()); hr == S_OK) {
-		ComPtr<IShellItem> shi;
-		fd->GetResult(shi.pptr());
-		return _shellItemPath(shi);
-	} else if (hr == HRESULT_FROM_WIN32(ERROR_CANCELLED)) {
-		return std::nullopt;
-	} else [[unlikely]] {
-		throw std::system_error(hr, std::system_category(), "IModalWindow::Show failed");
-	}
-}
-
-int Dialog::msgBox(std::wstring_view title, std::wstring_view mainInstruction,
-	std::wstring_view body, int tdcbfButtons, LPWSTR tdIcon) const
-{
-	TASKDIALOGCONFIG tdc = {
-		.cbSize = sizeof(TASKDIALOGCONFIG),
-		.hwndParent = hWnd(),
-		.dwFlags = TDF_ALLOW_DIALOG_CANCELLATION | TDF_POSITION_RELATIVE_TO_WINDOW,
-		.dwCommonButtons = tdcbfButtons,
-		.pszWindowTitle = title.data(),
-		.pszMainIcon = tdIcon,
-		.pszMainInstruction = mainInstruction.empty() ? nullptr : mainInstruction.data(),
-		.pszContent = body.data(),
-	};
-
-	int pnButton = 0;
-	if (HRESULT hr = TaskDialogIndirect(&tdc, &pnButton, nullptr, nullptr); FAILED(hr)) [[unlikely]] {
-		throw std::system_error(hr, std::system_category(), "TaskDialogIndirect failed");
-	}
-	return pnButton;
 }
--- wee/Dialog.h
+++ wee/Dialog.h
@@ -9,6 +9,32 @@ namespace lib {
 // Base to all dialog windows.
 class Dialog : public Window {
 public:
+	class SysDlgs final {
+	private:
+		friend Dialog;
+		Dialog* _pDlg = nullptr;
+		constexpr SysDlgs(Dialog *pDlg) : _pDlg{pDlg} { }
+	public:
+		SysDlgs() = delete;
+		SysDlgs(const SysDlgs&) = delete;
+		SysDlgs(SysDlgs&&) = delete;
+		SysDlgs& operator=(const SysDlgs&) = delete;
+		SysDlgs& operator=(SysDlgs&&) = delete;
+
+		[[nodiscard]] std::optional<std::wstring> openFile(std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const { return _openSave(true, false, namesExts); }
+		[[nodiscard]] std::optional<std::vector<std::wstring>> openFiles(std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const;
+		[[nodiscard]] std::optional<std::wstring> saveFile(std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const { return _openSave(false, false, namesExts); }
+		[[nodiscard]] std::optional<std::wstring> openFolder() const { return _openSave(true, true, {}); }
+		
+		// Calls TaskDialogIndirect(); returns IDOK, IDCANCEL, etc.
+		int msgBox(std::wstring_view title, std::wstring_view mainInstruction,
+			std::wstring_view body, int tdcbfButtons, LPWSTR tdIcon) const;
+
+	private:
+		[[nodiscard]] std::optional<std::wstring> _openSave(bool isOpen, bool isFolder,
+			std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const;
+	};
+
 	virtual ~Dialog() { }
 
 	constexpr Dialog() = default;
@@ -18,6 +44,9 @@ public:
 	Dialog& operator=(Dialog&&) = delete;
 
 protected:
+	// Exposes standard system modal dialogs.
+	SysDlgs sys{this};
+
 	virtual INT_PTR dlgProc(UINT uMsg, WPARAM wp, LPARAM lp) = 0; // to be overriden in user class
 	static INT_PTR CALLBACK _DlgProc(HWND hDlg, UINT uMsg, WPARAM wp, LPARAM lp);
 
@@ -26,17 +55,6 @@ protected:
 
 	// Calls EnableWindow() for each child control.
 	void enable(std::initializer_list<WORD> ctrlIds, bool doEnable = true) const;
-
-	[[nodiscard]] std::optional<std::wstring> showOpenFile(std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const { return _showOpenSave(true, false, namesExts); }
-	[[nodiscard]] std::optional<std::vector<std::wstring>> showOpenFiles(std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const;
-	[[nodiscard]] std::optional<std::wstring> showSaveFile(std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const { return _showOpenSave(false, false, namesExts); }
-	[[nodiscard]] std::optional<std::wstring> showOpenFolder() const { return _showOpenSave(true, true, {}); }
-	[[nodiscard]] std::optional<std::wstring> _showOpenSave(bool isOpen, bool isFolder,
-		std::initializer_list<std::pair<std::wstring_view, std::wstring_view>> namesExts) const;
-
-	// Calls TaskDialogIndirect(); returns IDOK, IDCANCEL, etc.
-	int msgBox(std::wstring_view title, std::wstring_view mainInstruction,
-		std::wstring_view body, int tdcbfButtons, LPWSTR tdIcon) const;
 
 private:
 	Window::_hWndPtr;

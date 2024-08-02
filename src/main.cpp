813e80b1143aa1c63ef62d65adfdfdcb11ec26e5
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-08-02 11:40:19 -0300

    Paths extracted into INI file.

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,41 @@
 #include <windlg/lib.h>
 using std::vector, std::wstring, std::wstring_view;
 
-static auto dirLibWinDlg = L"D:\\Stuff\\Core\\cpp\\windlg\\windlg";
-static auto baseDirTargets = L"D:\\Stuff\\Core\\cpp";
-static auto targets = {
-	L"flac-lame-frontend",
-	L"id3-fit",
-	L"king",
-	L"upd-windlg",
-	L"vscode-font-patch",
-};
-
 void processSolution(wstring_view target, wstring_view baseDirTargets, wstring_view dirLibWinDlg);
 void processFilter(wstring_view target, wstring_view baseDirTargets, wstring_view dirLibWinDlg);
 
+struct Config final {
+	wstring pathLibWinDlg;
+	wstring pathBaseTargets;
+	vector<wstring> programs;
+};
+
+static Config _loadIniConfig()
+{
+	auto iniPath = lib::str::fmt(L"%s\\upd-windlg.ini", lib::path::exeDir());
+	Config c = {
+		.pathLibWinDlg = lib::ini::readStr(iniPath, L"Paths", L"libWinDlg"),
+		.pathBaseTargets = lib::ini::readStr(iniPath, L"Paths", L"baseTargets"),
+	};
+
+	UINT i = 0;
+	for (;;) {
+		try {
+			c.programs.emplace_back( lib::ini::readStr(iniPath, L"Programs", lib::str::fmt(L"p%d", i++)) );
+		} catch (const std::invalid_argument&) {
+			break;
+		}
+	}
+	return c;
+}
+
 int main()
 {
-	for (auto&& target : targets) {
-		std::wcout << L"Project " << target << L"..." << std::endl;
-		processSolution(target, baseDirTargets, dirLibWinDlg);
-		processFilter(target, baseDirTargets, dirLibWinDlg);
+	Config c = _loadIniConfig();
+	for (auto&& program : c.programs) {
+		std::wcout << L"Updating " << program << L"..." << std::endl;
+		processSolution(program, c.pathBaseTargets, c.pathLibWinDlg);
+		processFilter(program, c.pathBaseTargets, c.pathLibWinDlg);
 	}
 
 	std::wcout << L"Press any key...";

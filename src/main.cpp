27f6cb5bbc371f15b65fd7ce1fa4a2a107558efb
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-08-02 01:32:55 -0300

    Whole program, 1st try.

---
+++ src/main.cpp
@@ -0,0 +1,109 @@
+#include <conio.h>
+#include <iostream>
+#include <stdexcept>
+#include <windlg/lib.h>
+
+static auto dirWinDlg = L"D:\\Stuff\\Core\\cpp\\windlg\\windlg";
+static auto dirTargets = L"D:\\Stuff\\Core\\cpp";
+static auto targets = {
+	L"flac-lame-frontend",
+	L"king",
+	L"upd-windlg",
+	L"vscode-font-patch",
+};
+
+void processSolution(std::wstring_view target)
+{
+	auto solutionPath = lib::str::fmt(L"%s\\%s\\%s.vcxproj", dirTargets, target, target);
+	auto lines = lib::str::splitLines(lib::FileMapped::ReadAllStr(solutionPath));
+
+	auto refill = [](const std::vector<std::wstring>& lines,
+			std::wstring_view tagSuffix,
+			std::wstring_view ext) -> std::vector<std::wstring> {
+		size_t idxLine0 = 0, idxLinePast = 0;
+		for (size_t i = 0; i < lines.size(); ++i) {
+			if (!idxLine0) { // still searching the beginning of block
+				if (lib::str::startsWith(lines[i], lib::str::fmt(L"    <Cl%s Include=\"..\\windlg\\windlg\\", tagSuffix))
+						&& lib::str::endsWith(lines[i], lib::str::fmt(L".%s\" />", ext)) ) {
+					idxLine0 = i;
+				}
+			} else { // now searching first line after block
+				if (!lib::str::startsWith(lines[i], lib::str::fmt(L"    <Cl%s Include=\"..\\windlg\\windlg\\", tagSuffix))) {
+					idxLinePast = i;
+					break;
+				}
+			}
+		}
+
+		if (!idxLine0 || !idxLinePast)
+			throw std::runtime_error("Solution line not found");
+
+		std::vector<std::wstring> newLines{lines.begin(), lines.begin() + idxLine0};
+		for (const auto& srcFile : lib::path::dirList( lib::str::fmt(L"%s\\*.%s", dirWinDlg, ext) )) {
+			newLines.emplace_back(lib::str::fmt(L"    <Cl%s Include=\"..\\windlg\\windlg\\%s\" />",
+				tagSuffix, lib::path::fileFrom(srcFile)));
+		}
+		newLines.insert(newLines.end(), lines.begin() + idxLinePast, lines.end());
+		return newLines;
+	};
+
+	lines = refill(lines, L"Compile", L"cpp");
+	lines = refill(lines, L"Include", L"h");
+	lib::File::EraseAndWriteStr(solutionPath, lib::str::join(lines, L"\r\n"));
+}
+
+void processFilter(std::wstring_view target)
+{
+	auto filterPath = lib::str::fmt(L"%s\\%s\\%s.vcxproj.filters", dirTargets, target, target);
+	auto lines = lib::str::splitLines(lib::FileMapped::ReadAllStr(filterPath));
+
+	auto refill = [](const std::vector<std::wstring>& lines,
+			std::wstring_view tagSuffix,
+			std::wstring_view ext) -> std::vector<std::wstring> {
+		size_t idxLine0 = 0, idxLinePast = 0;
+		for (size_t i = 0; i < lines.size(); ++i) {
+			if (!idxLine0) { // still searching the beginning of block
+				if (lib::str::startsWith(lines[i], lib::str::fmt(L"    <Cl%s Include=\"..\\windlg\\windlg\\", tagSuffix))
+						&& lib::str::endsWith(lines[i], lib::str::fmt(L".%s\">", ext)) ) {
+					idxLine0 = i;
+				}
+			} else { // now searching first line after block
+				i += 2; // will encompass 3 lines
+				if (!lib::str::startsWith(lines[i], lib::str::fmt(L"    <Cl%s Include=\"..\\windlg\\windlg\\", tagSuffix))) {
+					idxLinePast = i;
+					break;
+				}
+			}
+		}
+
+		if (!idxLine0 || !idxLinePast)
+			throw std::runtime_error("Filter line not found");
+
+		std::vector<std::wstring> newLines{lines.begin(), lines.begin() + idxLine0};
+		for (const auto& srcFile : lib::path::dirList( lib::str::fmt(L"%s\\*.%s", dirWinDlg, ext) )) {
+			newLines.emplace_back(lib::str::fmt(L"    <Cl%s Include=\"..\\windlg\\windlg\\%s\">",
+				tagSuffix, lib::path::fileFrom(srcFile)));
+			newLines.emplace_back(L"      <Filter>WinDlg</Filter>");
+			newLines.emplace_back(lib::str::fmt(L"    </Cl%s>", tagSuffix));
+		}
+		newLines.insert(newLines.end(), lines.begin() + idxLinePast, lines.end());
+		return newLines;
+	};
+
+	lines = refill(lines, L"Compile", L"cpp");
+	lines = refill(lines, L"Include", L"h");
+	lib::File::EraseAndWriteStr(filterPath, lib::str::join(lines, L"\r\n"));
+}
+
+int main()
+{
+	for (auto&& target : targets) {
+		std::wcout << L"Project " << target << L"..." << std::endl;
+		processSolution(target);
+		processFilter(target);
+	}
+
+	std::wcout << L"Press any key...";
+	auto noop = _getch();
+	return 0;
+}

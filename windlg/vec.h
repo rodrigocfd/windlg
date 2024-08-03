a99baa0c0f0e9891d97f753f4cd770313d89864b
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-08-03 11:34:45 -0300

    Better templates for vec functions.

--- windlg/vec.h
+++ windlg/vec.h
@@ -6,30 +6,36 @@
 
 namespace lib::vec {
 
-// Appends a single element to the vector. Same as push_back().
-template<typename T>
-void append(std::vector<T>& dest, const T& elem) {
+// Appends multiple elements to the vector with push_back().
+template<typename T,
+	typename E = std::vector<T>::value_type>
+void append(std::vector<T>& dest, const E& elem) {
 	dest.push_back(elem);
 }
-// Appends multiple elements to the vector.
-template<typename T, typename... U>
-void append(std::vector<T>& dest, const T& elem, const U&... elems) {
-	append(dest, elem);
-	append(dest, elems...);
-}
-// Appends all elements of vectors to the vector.
-template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>>
-void append(std::vector<T>& dest, R&& foreigner) {
-	dest.insert(dest.end(), foreigner.begin(), foreigner.end());
-}
-// Appends all elements of all vectors to the vector.
-template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>,
+// Appends multiple elements to the vector with push_back().
+template<typename T,
+	typename E = std::vector<T>::value_type,
 	typename... U>
-void append(std::vector<T>& dest, R&& foreigner, U... foreigners) {
-	append(dest, foreigner);
-	append(dest, foreigners...);
+void append(std::vector<T>& dest, const E& elem, U... rest) {
+	append(dest, elem);
+	append(dest, rest...);
+}
+
+// Appends all elements of vectors to the vector with insert().
+template<std::ranges::contiguous_range R,
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>> > // https://stackoverflow.com/q/78827063
+	requires std::ranges::sized_range<R>
+void append(std::vector<T>& dest, R&& other) {
+	dest.insert(dest.end(), other.begin(), other.end());
+}
+// Appends all elements of vectors to the vector with insert().
+template<std::ranges::contiguous_range R,
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>>,
+	typename... U>
+	requires std::ranges::sized_range<R>
+void append(std::vector<T>& dest, R&& other, U... rest) {
+	append(dest, std::forward<R>(other));
+	append(dest, rest...);
 }
 
 // Creates a vector and calls reserve().
@@ -42,15 +48,17 @@ template<typename T>
 
 // Returns the index of the first element.
 template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>>
-[[nodiscard]] std::optional<size_t> position(R&& v, const T& elem) {
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>> >
+	requires std::ranges::sized_range<R>
+[[nodiscard]] std::optional<size_t> position(R&& v, const std::type_identity_t<T>& elem) {
 	auto foundIt = std::find(v.begin(), v.end(), elem);
 	return (foundIt == v.end()) ? std::nullopt : std::optional{std::distance(v.begin(), foundIt)};
 }
 // Returns the index of the last element.
 template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>>
-[[nodiscard]] std::optional<size_t> positionRev(R&& v, const T& elem) {
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>> >
+	requires std::ranges::sized_range<R>
+[[nodiscard]] std::optional<size_t> positionRev(R&& v, const std::type_identity_t<T>& elem) {
 	auto foundIt = std::find(v.rbegin(), v.rend(), elem);
 	return (foundIt == v.rend()) ? std::nullopt : std::optional{std::distance(foundIt, std::prev(v.rend()))};
 }
@@ -58,7 +66,8 @@ template<std::ranges::contiguous_range R,
 // Example:
 // position_if(entries, [](const Entry&) -> bool { return true; });
 template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>>
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>> >
+	requires std::ranges::sized_range<R>
 [[nodiscard]] std::optional<size_t> positionIf(R&& v, std::predicate<T> auto pred) {
 	auto foundIt = std::find_if(v.begin(), v.end(), pred);
 	return (foundIt == v.end()) ? std::nullopt : std::optional{std::distance(v.begin(), foundIt)};
@@ -67,7 +76,8 @@ template<std::ranges::contiguous_range R,
 // Example:
 // position_if(entries, [](const Entry&) -> bool { return true; });
 template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>>
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>> >
+	requires std::ranges::sized_range<R>
 [[nodiscard]] std::optional<size_t> positionRevIf(R&& v, std::predicate<T> auto pred) {
 	auto foundIt = std::find_if(v.rbegin(), v.rend(), pred);
 	return (foundIt == v.rend()) ? std::nullopt : std::optional{std::distance(foundIt, std::prev(v.rend()))};
@@ -75,11 +85,12 @@ template<std::ranges::contiguous_range R,
 
 // Returns spans over the source vector, splitted by the delimiter, including empty spans.
 template<std::ranges::contiguous_range R,
-	typename T = std::ranges::range_value_t<R>>
-[[nodiscard]] std::vector<std::span<T>> split(R&& v, const T& delimiter) {
+	typename T = std::remove_reference_t<std::ranges::range_reference_t<R>> >
+	requires std::ranges::sized_range<R>
+[[nodiscard]] std::vector<std::span<T>> split(R&& v, const std::type_identity_t<T>& delimiter) {
 	if (v.empty()) return {};
 
-	std::span<T> src{v.begin(), v.end()};
+	std::span<T> src{v};
 	std::vector<std::span<T>> ret;
 	size_t head = 0;
 	for (;;) {
